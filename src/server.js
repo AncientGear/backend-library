5a606cb5f249182ccd85f9f998e788d47278794f
SaulTzakumOchoa <cpp.saul@gmail.com>
2020-03-06 21:43:00 -0600

    Adding PORT to the server

--- src/server.js
+++ src/server.js
@@ -2,3 +2,16 @@ const express = require('express');
 
 let app = express();
 
+const PORT = 3000;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// Routes
+
+
+// Server
+app.listen(PORT, () => {
+    console.log(`Listening port ${PORT}`);
+})
