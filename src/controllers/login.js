7527bb1da69a74167055aaaec495b7fdd71c2f81
SaulTzakumOchoa <cpp.saul@gmail.com>
2020-03-07 01:41:09 -0600

    Fixing bugs in login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -3,9 +3,28 @@ const {pool} = require('../database/connection');
 const login = (req, res) => {
     let body = req.body;
 
-    pool.query('SELECT * FROM ')
-    
-    res.send('Bienvenido al login');
+    await pool.query('SELECT * FROM ')
+        .then(queryResponse => {
+            if(queryResponse.rows.length == 0){
+                return res.status(400).send({
+                    ok: false,
+                    message: 'Usuario no encontrado',
+                })
+            }
+
+            return res.stauts(200).send({
+                ok: true,
+                user: 'usuer'
+            })
+        })
+        .catch(err => {
+            return res.status(500).send({
+                ok: false,
+                err: {
+                    message: 'Bad error to connect with server. Please, contact the Admin'
+                }
+            })
+        })
 }
 
 module.exports = {
